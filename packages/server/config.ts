6bfd44ed2925833c13437e29f908103dbd286e69
objStevo <hschalco@gmail.com>
2023-08-06 00:57:29 -0700

    update gitignore

---
+++ packages/server/config.ts
@@ -0,0 +1,6 @@
+require('dotenv').config();
+export const PORT = process.env.PORT || 8080;
+export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:8080';
+export const ADYEN_API_KEY = process.env.ADYEN_API_KEY || '';
+export const ADYEN_BASE_URL = process.env.ADYEN_BASE_URL || 'https://checkout-test.adyen.com';
+export const ADYEN_MERCHANT_ACCOUNT = process.env.ADYEN_MERCHANT_ACCOUNT || '';
