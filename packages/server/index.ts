291c58a4d4ee7484e3142567022c76a4cc42a9e5
objStevo <hschalco@gmail.com>
2023-08-07 22:04:05 -0700

    remove serving static files from backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,9 @@
 import cors from 'cors';
-import path from 'path';
 import express from 'express';
 
 import cookieParser from 'cookie-parser';
 
-import { PORT, CLIENT_ORIGIN } from './config';
+import { CLIENT_ORIGIN, PORT } from './config';
 import { checkoutRouter } from './routes';
 
 export const app = express();
@@ -23,22 +22,8 @@ app.use(
   })
 );
 
-// Issue is here, I am only sending back index.html when I should send back an file requested
-
-const root = path.join(__dirname, '../client', 'build');
-app.use(express.static(root));
-app.get('/', (req, res) => {
-  res.sendFile('index.html', { root });
-  console.log('requested index.html');
-});
-app.get('/:component', (req, res) => {
-  res.sendFile('index.html', { root });
-  console.log('requested component page');
-});
-
 app.use('/api/checkout', checkoutRouter);
 
 app.listen(PORT, () => {
   console.log('Your app is listening on port', PORT);
-  console.log('Client origin', CLIENT_ORIGIN);
 });
